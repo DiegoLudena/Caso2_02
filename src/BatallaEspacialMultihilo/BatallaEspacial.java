ee0fbc6d6a339523621d85a5bc6e5b7dba21da12
DiegoLudena <d_l_m88@yahoo.es>
2024-02-07 13:06:44 +0100

    Hilos funcionan

--- src/BatallaEspacialMultihilo/BatallaEspacial.java
+++ src/BatallaEspacialMultihilo/BatallaEspacial.java
@@ -3,7 +3,39 @@ package BatallaEspacialMultihilo;
 public class BatallaEspacial {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//Necesito dos naves para la batalla
+		
+		NaveEstelar Zorg = new NaveEstelar("Zorg", 0);
+		NaveEstelar Blip = new NaveEstelar("Blip", 0);
+
+		//Como NaveEstelar implementa Runnable, tengo que crear un hilo para cada nave
+		
+		Thread hiloZorg; //= new Thread(Zorg);
+		Thread hiloBlip; //= new Thread(Blip);
+		
+		//Como quiero que la batalla dure 200 avances, creo un bucle para llamar 200 veces a los hilos
+		int rondas = 200; //Lo hago con una variable para que sea más fácil de cambiar. Se podría preguntar cuánto dura la batalla en lugar de usar el número que pone el ejercicio como ejemplo.
+		int cuentarondas=0;
+		while (cuentarondas<rondas) {
+			
+			//En cada ronda hay que crear un nuevo hilo y llamar a su start. De no hacerlo así da una excepción IllegalThreadStateException.
+            hiloZorg = new Thread(Zorg);
+            hiloBlip = new Thread(Blip);
+			hiloZorg.start();
+			hiloBlip.start();
+			//Hago que los hilos se esperen para que estén siempre en la misma ronda
+			try {
+				hiloZorg.join();
+				hiloBlip.join();
+			} catch (InterruptedException e) {
+
+				e.printStackTrace();
+			}
+			cuentarondas++;
+		}
+		System.out.println("La nave Zorg ha recorrido " + Zorg.getPosicion());
+		System.out.println("La nave Blip ha recorrido " + Blip.getPosicion());
+		
 
 	}
 
--- src/BatallaEspacialMultihilo/NaveEstelar.java
+++ src/BatallaEspacialMultihilo/NaveEstelar.java
@@ -1,5 +1,62 @@
 package BatallaEspacialMultihilo;
 
-public class NaveEstelar {
+public class NaveEstelar implements Runnable{
+	
+	//Cada nave necesita tener un bando y una posición. Creo un constructor para la nave, con sus getters y setters
+	private String bando;
+	private int posicion;
+	
+	
+	public NaveEstelar(String bando, int posicion) {
+		this.bando = bando;
+		this.posicion = posicion;
+	}
+	
+	
+
+	/*Creo el método Avanzar para que las naves avancen su posición una distancia aleatoria. Voy a hacer que sea entre 1 y 100 metros. 
+	 Para que los hilos no se superpongan entre sí, es necesario un monitor, así que hago el método synchronized*/
+	
+	public String getBando() {
+		return bando;
+	}
+
+
+
+	public void setBando(String bando) {
+		this.bando = bando;
+	}
+
+
+
+	public int getPosicion() {
+		return posicion;
+	}
+
+
+
+	public void setPosicion(int posicion) {
+		this.posicion = posicion;
+	}
+
+
+
+	public synchronized void Avanzar(int distancia) {
+		posicion += distancia;
+		System.out.println("La nave " + bando +" ha avanzado " +distancia +" metros!");
+	}
+	
+
+	//Como implementa Runnable, hay que hacer override del método run
+	@Override
+	public void run() {
+		//Run genera un número aleatorio y llama al método avanzar, usando ese número como argumento
+		int distancia = (int) (Math.random()*100)+1;
+		Avanzar(distancia);			
+		
+		
+	}
+	
+	
 
 }
